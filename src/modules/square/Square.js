a52ddf462187104cc4d0c3bb39da9c19252d52d7
mesahni <me.sahni@gmail.com>
2019-12-20 07:03:41 -0800

    Update Square.js

--- src/modules/square/Square.js
+++ src/modules/square/Square.js
@@ -1,1 +1,10 @@
+import React from 'react';
 
+function Square(props){
+    return (
+	    <button id="square_button" className="square" onClick={() => props.onClick()}>
+			{props.value}
+		</button>
+	)
+}
+export default Square;
